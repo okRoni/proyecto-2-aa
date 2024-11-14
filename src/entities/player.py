d43a64d49e71ff1cf2be574546e8ec314961e510
Ricardo <rarce@estudiantec.cr>
2024-11-14 15:32:52 -0600

    add_card_to_hand and is_blackjack methods of Player class

--- src/entities/player.py
+++ src/entities/player.py
@@ -7,7 +7,9 @@ from card import Card
 from deck import Deck
 import numpy as np
 
+
 class Player(ABC):
+
     def __init__(self):
         self.hand: list[Card] = []
         self.standing: bool = False
@@ -18,6 +20,7 @@ class Player(ABC):
         '''
         Player chooses what to do on their turn
         '''
+
         pass
 
     @abstractmethod
@@ -25,6 +28,7 @@ class Player(ABC):
         '''
         Player chooses to stand and end their turn
         '''
+
         pass
 
     @abstractmethod
@@ -32,47 +36,59 @@ class Player(ABC):
         '''
         Adds a card to the player's hand
         '''
+
         pass
 
     def get_hand_value(self) -> int:
         '''
         Returns the value of the player's hand
         '''
-        pass
 
-    def add_card_to_hand(self, card) -> None:
+        hand_value = 0
+        for card in self.hand:
+            hand_value += card.value
+        return hand_value
+
+    def add_card_to_hand(self, card: Card) -> None:
         self.hand.append(card)
 
     def reset(self) -> None:
         '''
         Resets the player's game state
         '''
+
         self.hand = []
         self.standing = False
         self.busted = False
 
-    def get_hand(self) -> list:
+    def get_hand(self) -> list[Card]:
         '''
         Sends the player's hand
         '''
+
         return self.hand
 
     def is_busted(self) -> bool:
         '''
         Returns True if the player's hand value is greater than 21
         '''
-        pass
+
+        return True if self.get_hand_value() > 21 else False
 
     def is_blackjack(self) -> bool:
         '''
         Returns True if the player's hand value is 21
         '''
-        pass
+
+        if self.get_hand_value() == 21:
+            return True
+        return False
 
     def is_standing(self) -> bool:
         '''
         Returns True if the player is standing
         '''
+
         return self.standing
 
 
