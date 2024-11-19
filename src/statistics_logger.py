15cf977bd5c03de26c77a6665ef649d66915b0e1
Ricardo <rarce@estudiantec.cr>
2024-11-18 18:18:24 -0600

    Methods for load and store to json file and testing

--- src/statistics_logger.py
+++ src/statistics_logger.py
@@ -1,13 +1,15 @@
 '''
-Classes used to log the moves made by each player in the actual game session.
+Classes used to log the moves made by each player and the winner of each game.
 This data is used later to generate game statistics in the app.
-Terminology conventions:
- - Game session: Isolated set of all games played while the app is open.
- - Game: Like the Spanish 'partida'. Didn't find a better translation.
+Here 'game' means like the Spanish 'partida'. Didn't find a better translation.
 Available moves (hit and stand) are represented with the strings 'H' and 'S'.
 Players (croupier, ai1, ai2 and the human) are represented with the strings
 'croupier', 'ai1', 'ai2' and 'human', respectively.
 '''
+
+
+from typing import Any
+import json
 
 
 class Game:
@@ -22,11 +24,83 @@ class Game:
         self.human_moves: list[str] = []
         self.winner: str = ''
 
+
 class StatisticsLogger:
     '''
-    Holds a record of all games in the actual game sessions and saves it to
-    a .json file when the app is closed.
+    Holds a record of all games and saves it to static/games.json when the app
+    is closed.
     '''
 
     def __init__(self) -> None:
-        self.game_sessions
+        self.games: list[dict[str, Any]] = []
+        self.current_game: Game = Game()
+        self.load_data()
+
+    def load_data(self) -> None:
+        '''
+        Loads the content of static/games.json file to self.games.
+        '''
+
+        with open('../static/games.json', 'r', encoding='utf-8') as file:
+            self.games = json.load(file)
+
+    def store_data(self) -> None:
+        '''
+        Stores the content of self.games to static/games.json file.
+        '''
+
+        with open('../static/games.json', 'w', encoding='utf-8') as file:
+            json.dump(self.games, file)
+
+    def add_current_game(self) -> None:
+        '''
+        Appends the current game in the games list.
+        '''
+
+        game_dict: dict[str, list[str] | str] = {
+            'croupier_moves': self.current_game.croupier_moves,
+            'ai1_moves': self.current_game.ai1_moves,
+            'ai2_moves': self.current_game.ai2_moves,
+            'human_moves': self.current_game.human_moves,
+            'winner': self.current_game.winner
+        }
+        self.games.append(game_dict)
+        self.current_game = Game()
+
+    def log_move(self, entity: str, move: str) -> None:
+        '''
+        Logs the specified move of the specified entity in the current game.
+        '''
+
+        if move not in ['H', 'S']:
+            print(f'WARNING: Unknown move found in log_move ({move}).')
+            return
+
+        match entity:
+            case 'croupier':
+                self.current_game.croupier_moves.append(move)
+            case 'ai1':
+                self.current_game.croupier_moves.append(move)
+            case 'ai2':
+                self.current_game.croupier_moves.append(move)
+            case 'human':
+                self.current_game.croupier_moves.append(move)
+            case _:
+                print(f'WARNING: Unknown entity found in log_move ({entity}).')
+
+    def log_winner(self, winner: str) -> None:
+        '''
+        Logs the specified winner in the current game.
+        '''
+
+        self.current_game.winner = winner
+
+
+if __name__ == '__main__':
+    logger = StatisticsLogger()
+    logger.log_move('croupier', 'S')
+    logger.log_move('ai1', 'S')
+    logger.log_move('ai2', 'S')
+    logger.log_move('human', 'S')
+    logger.log_winner('human')
+
