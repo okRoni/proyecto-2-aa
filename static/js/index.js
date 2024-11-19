3d5e3e7f7dc0a8801bd7ac5e49d0ddb51dcffdf6
Oscar Roni <oscar.gt.140.ro@gmail.com>
2024-11-18 20:54:50 -0600

    Basic player rendering

--- static/js/index.js
+++ static/js/index.js
@@ -3,3 +3,15 @@ import Player from './player.js';
 // For the moment we are importing the socket.io.js file from the CDN
 var socket = io();
 
+
+const player = new Player('Player', Player.positions.player);
+let testCards = [
+  { value: 2, fileName: '2_of_clubs.png', color: 'black' },
+  { value: 3, fileName: '3_of_clubs.png', color: 'black' },
+  { value: 4, fileName: '4_of_clubs.png', color: 'red' },
+  { value: 11, fileName: 'ace_of_clubs.png', color: 'black' }
+];
+player.hand = testCards;
+player.render();
+
+
--- static/js/player.js
+++ static/js/player.js
@@ -1,4 +1,4 @@
-class Player {
+export default class Player {
     static players = [];
     static positions = {
         player: 'player',
@@ -11,7 +11,8 @@ class Player {
         return Player.players.find(player => player.position === position);
     }
 
-    constructor(position) {
+    constructor(name, position) {
+        this.name = name;
         this.position = position;
         this.hand = [];
         this.busted = false;
@@ -19,25 +20,62 @@ class Player {
         this.hideHand = false;
     }
 
+    /**
+     * Render the player into its respective container
+     */
     render() {
-        this.renderPlayer();
+        this.renderPlayerBase();
         this.renderHand();
     }
 
-    renderPlayer() {
-        const playerContainer = document.createElement('div');
-        const player = 
-        player.classList.add('player');
-        player.id = this.position;
-        player.innerHTML = `
+    /**
+     * Render the player base structure.
+     * This includes the player name, score and hand container
+     */
+    renderPlayerBase() {
+        const playerContainer = document.getElementById(this.position);
+        playerContainer.innerHTML = `
             <div class="player-info">
-                <div class="player-name">${this.position}</div>
+                <div class="player-name">${this.name}</div>
                 <div class="player-score">0</div>
             </div>
             <div class="player-hand"></div>
         `;
     }
 
+    /**
+     * Render the player hand
+     * This will render the cards in the player's hand
+     */
     renderHand() {
+        const handElement = this.getPlayerElement('player-hand');
+        handElement.innerHTML = '';
+        this.hand.forEach(card => {
+            this.renderCard(card);
+        });
+    }
+
+    /**
+     * Render a card into the player's hand
+     * @param {Object} card card object in format `{ value, fileName, color }`
+     */
+    renderCard(card) {
+        const handElement = this.getPlayerElement('player-hand');
+        const cardContainer = document.createElement('div');
+        cardContainer.classList.add('card-container');
+        handElement.appendChild(cardContainer);
+        const cardElement = document.createElement('img');
+        cardElement.src = `../static/deck_scans/${card.fileName}`;
+        cardElement.classList.add('card-img');
+        handElement.appendChild(cardElement);
+    }
+
+    /**
+     * Gets a element by class name from the player container
+     * @param {string} className class name of the element to get
+     * @returns {HTMLElement}
+     */
+    getPlayerElement(className) {
+        return document.querySelector(`#${this.position} .${className}`);
     }
 }
